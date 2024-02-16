895518b70db6c710ce82d4698171aa00e3857082
elizabethmck <elizabeth.mckeeby@students.fhu.edu>
2024-02-15 21:07:20 -0600

    Update main.py
    Test #1

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(list1, list2):
+    list1 = list1.upper()
+    list1 = list1.replace(" ","")
+    list2 = list2.upper()
+    list2 = list2.replace(" ","")
+
+    
+    list1 = []
+    for i in list1:
+        list1.append(i)
+    
+    for i in list1:
+        if i in list2:
+            continue
+        else:
+            return False
+    return True
+
 
 if __name__ == '__main__':
     #REMOVE PASS YOUR CODE GOES HERE
-    pass
-    
+    list1 = input()
+    list2 = input()
+    print(anagram(list1, list2))
